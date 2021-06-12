fc6b705c2c9794f9c61fa5f2e1827f3f7cc8a48a
unknown <kunal.kumar@livnsense.com>
2021-06-12 12:09:27 +0530

    Initial Authors Routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //     ejs
 //     express-ejs-layouts
 //     mongoose
+//     body-parser - to access different input elements from server
 
 
 // Checking the environment is prod or dev
@@ -15,14 +16,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 // Database Connection
 const mongoose = require('mongoose')
@@ -32,5 +36,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log("Connected to Mongoose"))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
